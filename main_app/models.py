bb069626516513153d7257e7b8347bcc19624e3d
Kris Mally <kristophermally@gmail.com>
2022-07-09 20:09:42 -0600

    Display list of characters and add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
+from unicodedata import name
 from django.db import models
 
+
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
+    ability_score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from main_app.models import Character
+
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def character_index(request):
+    chars = Character.objects.all()
+    return render(request, 'characters/index.html', { 'chars': chars })
+    
